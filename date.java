0ac8638b329dcaca73b7a338c3769bab5aa69112
ghost8202 <chrishall8202@hotmail.com>
2016-02-04 20:22:40 -0500

    started 2_5

---
+++ date.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+
+public class date extends JFrame {
+	date(){
+		setTitle("Expiration Date");
+		JLabel label= new JLabel("Expiration Date:");
+		setDefaultCloseOperation(date.EXIT_ON_CLOSE);
+	getContentPane().add(label);
+	}
+
+}
---
+++ main1.java
@@ -0,0 +1,10 @@
+
+public class main1 {
+	public static void main(String[] args){
+		date d= new date();
+		d.setVisible(true);
+		d.pack();
+		
+	}
+
+}
